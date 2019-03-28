a2d6309bf6961a60bfed9486ae2f5d6eaf496627
Daniel Gomez <dggomez21@gmail.com>
2019-03-28 13:11:37 -0400

    Update finance.py

--- AI/finance.py
+++ AI/finance.py
@@ -9,4 +9,17 @@ style.use('ggplot')
 start = dt.datetime(2000,1,1)
 end = dt.datetime(2019,1,1)
 df =  web.DataReader('ACB', 'yahoo', start, end)
-print(df.head())
+df.to_csv('ACB.csv')
+# df = pd.read_csv('ACB.csv', parse_dates=True, index_col=0)
+# print(df.head())
+df.plot()
+# df['Adj Close'].plot()
+
+#draw line
+# ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1)
+# ax1 = plt.subplot2grid((6,1),(0,0), rowspan=5, colspan=1, sharex=ax2)
+# ax1.plot(df.index, df['Adj Close'])
+# ax1.plot(df.index, df['100ma'])
+
+
+plt.show()
