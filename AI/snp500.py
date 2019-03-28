fd5b61cd4d22f68734f32316371d09e3fb148699
Daniel Gomez <dggomez21@gmail.com>
2019-03-28 14:26:37 -0400

    Update snp500.py

--- AI/snp500.py
+++ AI/snp500.py
@@ -1,4 +1,8 @@
 import bs4 as bs
+import datetime as dt
+import os
+import pandas as pd
+import pandas_datareader.data as web
 import pickle
 import requests
 
@@ -16,4 +20,24 @@ def save_sp500_tickers():
    print(tickers)
    return tickers
    
-save_sp500_tickers()
+#save_sp500_tickers()
+
+def get_data_from_yahoo(reload_sp500=False):
+    if reload_sp500:
+       tickers = save_sp500_tickers()
+    else:
+        with open("sp500tickers.pickle", "rb") as f:
+          tickers = pickle.load(f)
+    if not os.path.exists('stock_dfs'):
+        os.makedirs('stock_dfs')
+    start = dt.datetime(2000,1,1)
+    end = dt.datetime(2018,1,1)
+    
+    for ticker in tickers:
+      if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
+        df = web.DataReader(ticker, 'yahoo', start, end)
+        df.to_csv('stock_dfs/{}.csv'.format(ticker))
+      else:
+        print('Already have {}'.format(ticker))
+        
+get_data_from_yahoo()   
